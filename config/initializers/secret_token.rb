1450f22a7a14e972eec5113a76e3a4661cd3a481
Reece1984 <jamesreece@live.co.uk>
2012-10-16 15:25:36 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gobeyond::Application.config.secret_token = 'b72946d273c3eeffc2f0b6b5f90e7a95a822155f4c91afd35f18d13f38363e94de33c0c79ac63729b2f790dd5509d5b2cbab8164a53f3c718a228e98c612352e'
